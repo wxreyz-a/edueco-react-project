6af88b05e66cc94dd715001c7418ad5802be68f5
wxreyz-a <arishocini3@gmail.com>
2026-02-23 23:34:38 +0100

    Removes

--- src/LeLivre/PageDesChapitres.test.js
+++ src/LeLivre/PageDesChapitres.test.js
@@ -11,14 +11,14 @@ describe('ChapterIndex Component', () => {
       </BrowserRouter>
     );
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(/Explorez l'Histoire et l'Impact de l'Argent/i);
-    expect(screen.getByRole('heading', { name: /Chapitre 1 : Une brève histoire de l'argent/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /Chapitre 1 : Une brève histoire de l'argent/i })).toHaveAttribute('href', '/parties-chapitre-un');
+    expect(screen.getByRole('heading', { name: /Chapitre 1 :/i })).toHaveTextContent(/histoire de l/i);
+    expect(screen.getByRole('link', { name: /Chapitre 1 :/i })).toHaveAttribute('href', '/parties-chapitre-un');
     expect(screen.getByRole('heading', { name: /Chapitre 2 : Capitalisme et Mondialisation/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /Chapitre 2 : Capitalisme et Mondialisation/i })).toHaveAttribute('href', '/parties-chapitre-deux');
     expect(screen.getByRole('heading', { name: /Chapitre 3 : Investissement, Bourse et Cryptomonnaies/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /Chapitre 3 : Investissement, Bourse et Cryptomonnaies/i })).toHaveAttribute('href', '/parties-chapitre-trois');
-    expect(screen.getByRole('heading', { name: /Chapitre 4 : La Psychologie de l'Argent/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /Chapitre 4 : La Psychologie de l'Argent/i })).toHaveAttribute('href', '/parties-chapitre-quatre');
+    expect(screen.getByRole('heading', { name: /Chapitre 4 : Psychologie/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /Chapitre 4 : Psychologie/i })).toHaveAttribute('href', '/parties-chapitre-quatre');
   });
 
   test('renders chapter descriptions', () => {
@@ -30,3 +30,5 @@ describe('ChapterIndex Component', () => {
     expect(screen.getByText(/Découvrez comment l'argent a évolué, du troc aux premières monnaies./i)).toBeInTheDocument();
   });
 });
+
+
